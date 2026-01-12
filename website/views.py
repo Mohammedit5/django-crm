4c9f14e98aa8f43f388e42b038038d3b6c326c23
Mohammed Musa <mmusa3914@gmail.com>
2026-01-12 21:59:31 +0400

    registrations2

--- website/views.py
+++ website/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render , redirect
 from django.contrib.auth import authenticate, login, logout 
-
+from .form import SignUpForm
 #from django.contrib.auth import messages
 
 # where to impoort messages from
@@ -42,7 +42,19 @@ def logout_user(request):
  #   pass
 
 def register_user(request):
-     return render(request, 'register.html')    
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data('username')
+            password = form.cleaned_data('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "Registration successful." )
+            return redirect('home')
+        else:
+            form = SignUpForm()
+            return render(request, 'register.html' , {'form': form} )    
 
 
 
